b9a5bb44b2f58b3ee08b81c543f6e227dc18c1da
user <andreykrendzel@hotmail.com>
2022-11-29 14:49:21 +0200

    release semantcs

---
+++ release.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    branches: "master",
+    repositoryUrl: "https://github.com/andrey-krendzel/react-app",
+    plugins: ['@semantic-release/commit-analyzer',
+'@semantic-release/release-notes-generator',
+'@semantic-release/github']
+}

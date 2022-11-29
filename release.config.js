372273de922310a8525b628f8e0fc3913fad8cee
user <andreykrendzel@hotmail.com>
2022-11-29 16:17:33 +0200

    fix: fix

--- release.config.js
+++ release.config.js
@@ -2,6 +2,11 @@ module.exports = {
     branches: "master",
     repositoryUrl: "https://github.com/andrey-krendzel/react-app",
     plugins: ['@semantic-release/commit-analyzer',
-'@semantic-release/release-notes-generator',
-'@semantic-release/github']
+        '@semantic-release/release-notes-generator',
+        ['@semantic-release/github', {
+            assets: [
+            {path: "build.zip", label: "Build"},
+            {path: "coverage.zip", label: "Coverage"}
+        ]}]
+    ]
 }
